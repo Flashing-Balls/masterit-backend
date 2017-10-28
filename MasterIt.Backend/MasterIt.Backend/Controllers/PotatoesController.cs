723ba52a8b570d8f621033b55893a14b5f681fed
Damian Tarnacki <warnaa@gmail.com>
2017-10-28 17:47:23 +0200

    Initial project with /api/potato endpoint

---
+++ MasterIt.Backend/MasterIt.Backend/Controllers/PotatoesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MasterIt.Backend.Controllers
+{
+    public class PotatoesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "potato01", "potato02" };
+        }
+    }
+}
