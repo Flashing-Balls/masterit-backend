bb1f0e2e45d274b4a1e398d45c02240051bf87a2
Damian Tarnacki <warnaa@gmail.com>
2017-10-29 00:21:03 +0200

    Extended Post model

--- MasterIt.Backend/MasterIt.Backend/Models/Post.cs
+++ MasterIt.Backend/MasterIt.Backend/Models/Post.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MasterIt.Backend.Models
@@ -7,8 +8,10 @@ namespace MasterIt.Backend.Models
     {
         [Key]
         public int Id { get; set; }
-
+        public DateTime Date { get; set; }
         public int VideoUrl { get; set; }
+        public bool IsRatable { get; set; }
+        public bool IsApproved { get; set; }
         public User User { get; set; }
         public Skill Skill { get; set; }
         public IEnumerable<Comment> Comments { get; set; }
