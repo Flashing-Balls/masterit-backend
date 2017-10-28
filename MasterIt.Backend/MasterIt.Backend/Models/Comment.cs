be92c85749d3a869095d876eb342d6fb36399a32
Damian Tarnacki <warnaa@gmail.com>
2017-10-28 22:43:39 +0200

    Add missing reference Comment to Post

--- MasterIt.Backend/MasterIt.Backend/Models/Comment.cs
+++ MasterIt.Backend/MasterIt.Backend/Models/Comment.cs
@@ -10,6 +10,7 @@ namespace MasterIt.Backend.Models
 
         public DateTime Date { get; set; }
         public string Content { get; set; }
+        public Post Post { get; set; }
         public User User { get; set; }
     }
 }
