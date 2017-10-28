2da83d7b533ccd99aa0e9ad6d956e54346136dd8
Damian Tarnacki <warnaa@gmail.com>
2017-10-28 20:25:29 +0200

    Enable CORS

--- MasterIt.Backend/MasterIt.Backend/Controllers/UsersController.cs
+++ MasterIt.Backend/MasterIt.Backend/Controllers/UsersController.cs
@@ -10,9 +10,11 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using MasterIt.Backend;
 using MasterIt.Backend.Models;
+using System.Web.Http.Cors;
 
 namespace MasterIt.Backend.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
         private DatabaseContext db = new DatabaseContext();
